674697b324d7ae72f85a1fae30586a72720d07b0
lemme11111 <123477941+lemme11111@users.noreply.github.com>
2023-04-18 16:55:23 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,13 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'postgres',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'blue',
-    database : 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    host : process.env.DATABASE_HOST,
+    port: 5432,
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PW,
+    database : process.env.DATABASE_DB
   }
 });
 
