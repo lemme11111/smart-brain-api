88a978cb9cf40b9c055f6ea1900fd6b6c0dcf2ac
lemme11111 <123477941+lemme11111@users.noreply.github.com>
2023-04-18 15:17:01 +0800

    Done

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'd7e89c4f168145059b4ca1fe0928c587'
+});
+
+const handleApiCall = (req, res) => {
+  app.models.predict('face-detection', req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+ 	 const { id } = req.body;
+ 	 db('users').where('id', '=', id)
+  	.increment('entries', 1)
+  	.returning('entries')
+ 	.then(entries => {
+ 	   res.json(entries[0].entries);
+ 	 })
+	  .catch(err => res.status(400).json('unable to get entries'))
+}
+
+
+module.exports = {
+	handleImage,
+	handleApiCall
+}
