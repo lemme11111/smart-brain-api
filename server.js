e8f7bcb9752552a88714bc3317a664a5b682308e
lemme11111 <123477941+lemme11111@users.noreply.github.com>
2023-04-19 08:46:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'postgres',
   connection: {
-    connectionString : process.env.DATABASE_URL
+    connectionString : process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
     host : process.env.DATABASE_HOST,
     port: 5432,
